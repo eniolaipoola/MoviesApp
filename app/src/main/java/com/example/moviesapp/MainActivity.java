da9ae0bbe558412c56dbfb28e90a52f8415ed86c
IPOOLA Eniola <eniolaipoola@gmail.com>
2019-07-07 17:25:50 +0100

    Add settings menu to toggle the sort order of the movies

--- app/src/main/java/com/example/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/moviesapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.moviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemSelected = item.getItemId();
+        if(itemSelected == R.id.most_popular){
+            Toast.makeText(this, "Most popular movies here", Toast.LENGTH_SHORT).show();
+        } else if (itemSelected == R.id.top_rated){
+            Toast.makeText(this, "Highest rating movies here", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/moviesapp/utils/APPCOnstant.java
@@ -0,0 +1,4 @@
+package com.example.moviesapp.utils;
+
+public class APPCOnstant {
+}
